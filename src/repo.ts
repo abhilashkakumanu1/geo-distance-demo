2afaeb391c4eee7a89ffc654e96fb7435ad5501c
Abhilash Kakumanu <abhilashkakumanu1@gmail.com>
2024-03-27 16:47:59 +0530

    added real query to find haversine distance

--- src/repo.ts
+++ src/repo.ts
@@ -1,11 +1,25 @@
 import { PrismaClient } from "@prisma/client";
-import { IPlacesRepo } from "./types";
+import { IPlace, IPlacesRepo } from "./types";
 
 const prisma = new PrismaClient();
 
 export class PlacesRepo implements IPlacesRepo {
-  getPlacesWithInRadius = async (lat, lon, radius) => {
-    const places = await prisma.places.findMany();
+  getPlacesWithInRadius = async (
+    lat: number,
+    lon: number,
+    radius: number
+  ): Promise<IPlace[]> => {
+    const places: IPlace[] = await prisma.$queryRaw`
+      SELECT * 
+      FROM "Places"
+      WHERE acos(
+        sin(radians(${lat})) 
+        * sin(radians(latitude)) 
+        + cos(radians(${lat})) 
+        * cos(radians(latitude)) 
+        * cos(radians(${lon}) - radians(longitude))
+      ) * 6371 <= ${radius};
+    `;
     return places;
   };
 }
