b0ae48d4f812996f0969ce44a4aaa74da6f71804
Abhilash Kakumanu <abhilashkakumanu1@gmail.com>
2024-03-27 14:07:29 +0530

    moved routing logic into routes.ts file

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,10 @@
 import express from "express";
+import router from "./routes";
 
 const app = express();
 const PORT = 8080;
 
-app.get("/", (_, res) => {
-  res.send("Hello Readers!");
-});
+app.use("/", router);
 
 app.listen(PORT, () => {
   return console.log(
